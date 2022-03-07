ac8993ff82272bf624be32033d1766c64dc59215
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2022-03-07 19:08:45 -0300

    fotter styled

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,7 +1,22 @@
+import { Box, Container, Stack, Text } from "@chakra-ui/react";
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Container maxW="container.xl">
+      <Stack padding="3" bg="purple.900">
+        <Text
+          alignSelf="center"
+          color="gray.50"
+          fontWeight="semibold"
+          mt={3}
+          textAlign="center"
+        >
+          Made with love by Mauricio Ojeda
+        </Text>
+      </Stack>
+    </Container>
+  );
 };
 
 export default Footer;
