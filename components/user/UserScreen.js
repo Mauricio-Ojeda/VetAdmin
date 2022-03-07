0371be972c2e30ae62199e26bcfe39a5c409366c
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2022-03-07 17:02:17 -0300

    Component userScreen created

---
+++ components/user/UserScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AddPatient from "./AddPatient";
+import PatientsList from "./PatientsList";
+
+const UserScreen = () => {
+  return (
+    <>
+      <AddPatient /> <PatientsList />
+    </>
+  );
+};
+
+export default UserScreen;
