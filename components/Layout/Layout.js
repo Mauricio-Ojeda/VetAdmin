964a9618cbc2b4df39404f4e17a4419fdc4a2bef
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2022-03-07 19:09:17 -0300

    container in layout

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,12 +1,15 @@
 import Navbar from "../header/NavBar";
 import Footer from "../footer/Footer";
+import { Container } from "@chakra-ui/react";
 
 export default function Layout({ children }) {
   return (
     <>
-      <Navbar />
-      <main>{children}</main>
-      <Footer />
+      <Container maxW="container.xl" centerContent p={0}>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </Container>
     </>
   );
 }
