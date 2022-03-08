1db168b412dccd462dd82dd07a9c12a6c373f93b
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2022-03-08 08:42:51 -0300

    main layout edited

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,14 +1,28 @@
 import Navbar from "../header/NavBar";
 import Footer from "../footer/Footer";
-import { Container } from "@chakra-ui/react";
+import { Container, Stack, VStack } from "@chakra-ui/react";
 
 export default function Layout({ children }) {
   return (
     <>
-      <Container maxW="container.xl" centerContent p={0}>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
+      <Container maxW="1440px" centerContent p={0}>
+        <VStack
+          maxW="1440px"
+          w="100vw"
+          h="100vh"
+          justify="center"
+          alignItems="center"
+        >
+          <Stack>
+            <Navbar />
+          </Stack>
+          <Stack h="100%" maxH="896px">
+            <main>{children}</main>
+          </Stack>
+          <Stack alignContent="baseline">
+            <Footer />
+          </Stack>
+        </VStack>
       </Container>
     </>
   );
