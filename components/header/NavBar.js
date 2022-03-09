c01c6635b9979d04be06263beb93b82d5f925ed3
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2022-03-09 16:23:14 -0300

    responsive navbar

--- components/header/NavBar.js
+++ components/header/NavBar.js
@@ -1,7 +1,35 @@
+import { Container, Stack, Text } from "@chakra-ui/react";
 import React from "react";
+import Logo from "./Logo";
+import MenuLinks from "./MenuLinks";
+import MenuToggle from "./MenuToggle";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <Container maxW="1440px" p={0}>
+      <Stack
+        as="nav"
+        direction="row"
+        bg="purple.900"
+        h="65px"
+        align="center"
+        justify="space-between"
+        wrap="wrap"
+        w="1440px"
+        p={3}
+        mb={8}
+        maxW="100vw"
+      >
+        <Logo alignSelf="center" justify="left" />
+        <MenuToggle toggle={toggle} isOpen={isOpen} />
+        <MenuLinks isOpen={isOpen} />
+      </Stack>
+    </Container>
+  );
 };
 
 export default NavBar;
